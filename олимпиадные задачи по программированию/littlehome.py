700c030a2a060ecc191876606cd82828d73ec35c
DJsivo <114789776+DJsivo@users.noreply.github.com>
2023-09-22 01:16:01 +0700

    Add files via upload

---
+++ олимпиадные задачи по программированию/littlehome.py
@@ -0,0 +1,28 @@
+x, y, l, c1, c2, c3, c4, c5, c6 = (int(input()) for i in range(9)) 
+summ = 0
+p = 2*(x+y) #периметр нового дома
+new = c4 + c5 #постройка из нового
+old = c2 + c3 #постройка из старого разрушенного
+snos = c2 + c6 #снос и вывоз
+if (old <= new + snos) and (c1 <= old):
+    if (l >= max(x,y)) and (l<p): 
+        summ = c1*max(x,y) + (p - l)*new + (l - max(x,y))*old
+    if l<max(x,y):
+        summ = l*c1 + new*(p-l)
+    if l>=p:
+        summ = max(x,y)*c1 + (p-max(x,y))*old + (l-p)*snos 
+if (old <= new + snos) and (c1 > old):
+    if l<p:
+        summ = l*old + (p-l)*new
+    if l>=p:
+        summ = p*old + (l-p)*snos
+if (old > new + snos) and (c1<= new + snos):
+    if (l >= max(x,y)) and (l<p):
+        summ = max(x,y)*c1 + (l-max(x,y))*(new+snos) + (p-l)*new
+    if l<max(x,y):
+        summ = l*c1 + (p-l)*new
+    if l>p:
+        summ = max(x,y)*c1 + (p-max(x,y))*(new+snos) + (l-p)*snos
+print(summ)
+
+
