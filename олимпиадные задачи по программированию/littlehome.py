c9f95f300a0326e907d69296c904cc5807fc4cd4
DJsivo <114789776+DJsivo@users.noreply.github.com>
2023-09-22 11:26:46 +0700

    Update littlehome.py

--- олимпиадные задачи по программированию/littlehome.py
+++ олимпиадные задачи по программированию/littlehome.py
@@ -1,9 +1,9 @@
 x, y, l, c1, c2, c3, c4, c5, c6 = (int(input()) for i in range(9)) 
 summ = 0
-p = 2*(x+y) #периметр нового дома
-new = c4 + c5 #постройка из нового
-old = c2 + c3 #постройка из старого разрушенного
-snos = c2 + c6 #снос и вывоз
+p = 2*(x+y) 
+new = c4 + c5 
+old = c2 + c3 
+snos = c2 + c6 
 if (old <= new + snos) and (c1 <= old):
     if (l >= max(x,y)) and (l<p): 
         summ = c1*max(x,y) + (p - l)*new + (l - max(x,y))*old
@@ -23,6 +23,9 @@ if (old > new + snos) and (c1<= new + snos):
         summ = l*c1 + (p-l)*new
     if l>p:
         summ = max(x,y)*c1 + (p-max(x,y))*(new+snos) + (l-p)*snos
+if (old > new + snos) and (c1 > new + snos):
+    if (l >= max(x,y)) and (l<p):
+        summ = l*snos + p*new
 print(summ)
 
 
