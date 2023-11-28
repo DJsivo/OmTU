ccbfc456d9a2bc6b4ee4100a14262726cf065d45
DJsivo <114789776+DJsivo@users.noreply.github.com>
2023-11-28 14:55:28 +0700

    Update max_length_increasing_subseq.cs

--- алгоритмизация и программирование/Задания от 19.09.23/For sub with n elements/max_length_increasing_subseq.cs
+++ алгоритмизация и программирование/Задания от 19.09.23/For sub with n elements/max_length_increasing_subseq.cs
@@ -3,22 +3,23 @@ class Posledovatelnost_n
 {
     static void Main()
     {
-        int n, a, b, longgest_podposled = 1, current_longgest_podposled = 1; //k - min podposledovat
+        int n, a, b, longgest_podposled = 1, current_longgest_podposled = 1;//m, k
         n = Convert.ToInt32(Console.ReadLine());
         a = Convert.ToInt32(Console.ReadLine());
-            for (int i = 1; i < n; i++)
+        for (int i = 1; i < n; i++)
         {
             b = Convert.ToInt32(Console.ReadLine());
             if (b > a)
             {
                 current_longgest_podposled++;
-                longgest_podposled = current_longgest_podposled;
+                
             }
-            else
-            {
-                current_longgest_podposled = 1;
-            }
-            a = b;
+            else if (current_longgest_podposled> longgest_podposled) 
+                {
+                    longgest_podposled = current_longgest_podposled;
+                    current_longgest_podposled = 1;
+                }
+             a = b;          
         }
         Console.WriteLine(longgest_podposled);
     }
