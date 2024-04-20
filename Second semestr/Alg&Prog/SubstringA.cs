bcb4ac3d2a7ded02d59ef60623b61c915aeae37c
DJsivo <114789776+DJsivo@users.noreply.github.com>
2024-04-20 18:37:02 +0700

    Add files via upload

---
+++ Second semestr/Alg&Prog/SubstringA.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+class Hz
+{
+    static void Main()
+    {
+        string shortestSubA = "";
+        int minSubLeng = int.MaxValue;
+        using (StreamReader file = new StreamReader(@"MyFile.txt"))
+        {
+            string line = file.ReadLine();
+            while (line != null)
+            {
+                int subLeng = 0;
+                foreach (char c in line)
+                {
+                    if (c == 'a') subLeng++;
+                }
+                if ((subLeng > 0) && (subLeng < minSubLeng))
+                {
+                    minSubLeng = subLeng;
+                    shortestSubA = line;
+                }
+            }
+        }
+        Console.WriteLine(@$"Строка с наименьшей длиной подпоследовательности 'а': 
+{shortestSubA}
+Длина подпоследовательности:
+{minSubLeng}");
+    }
+}
